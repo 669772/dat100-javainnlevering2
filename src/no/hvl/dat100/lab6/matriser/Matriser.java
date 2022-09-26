76d73b4818c4e710031ccb8b8bf4925e95c573f9
669817 <669817@stud.hvl.no>
2022-09-26 12:13:43 +0200

    Update Matriser.java

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -5,31 +5,74 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for (int[] navn : matrise ) {
+			
+			for (int verdi : navn) {
+				
+				System.out.print(verdi + " ");
+				
+			}
+			
+			System.out.println();
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String ut = "";
 		
+		for (int[] i : matrise) {
+			for (int verdi : i) {
+				ut += verdi + " " ;
+			}
+			ut += "\n";
+		}
+		return ut;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
+		int[][] nymat = new int[matrise.length][matrise[0].length];
+		
+		for (int i = 0; i < matrise.length; i++) {
+			
+			for (int y = 0; y < matrise[i].length; y++) {
+				nymat[i][y] = matrise[i][y] * tall;
+			}
+		}
+		
+		return nymat;
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean likL = true;
+		boolean like = true;
+		int i = 0;
+		
+		if (a.length != b.length) {
+			likL = false;
+		}
+		
+		while (i < a.length && like == true && likL == true) {
+			
+			int y = 0;
+			
+			while (y < a[i].length && like == true) {
+				
+				if (a[i][y] != b[i][y]) {
+					like = false;
+				}
+				
+				y++;
+			}
+			i++;
+		}
+		return like;
 	}
 	
 	// e)
